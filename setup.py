75156f5e2e59abad4fcdc118c4ec02d299b29ad6
Matthias Bach <matthias.bach@blue-yonder.com>
2015-07-09 15:13:29 +0200

    Include readme and license in sdist.
    Previously the sdist could not be installed as the setup.py would file because of the missing readme file.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 
 setup(
     name='devpi-plumber',
-    version='0.2.1',
+    version='0.2.2',
     packages=find_packages(exclude=['tests']),
     author='Stephan Erb',
     author_email='stephan.erb@blue-yonder.com',
